4a3a6ca95a8d5c7781c151264986275f9184759b
Mariya <maria_dolgova@inbox.ru>
2023-03-12 15:08:01 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from datetime import date
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+
+    current_date = date.today()
+    print(f'Дата : {current_date}')
